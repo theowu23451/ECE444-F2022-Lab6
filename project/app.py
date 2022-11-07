bcafd1c2a780c2913ac3401d5dbe09553ad40845
Theo Wu <theowu23451@gmail.com>
2022-11-07 11:32:48 -0500

    Setup database

--- project/app.py
+++ project/app.py
@@ -1,8 +1,45 @@
-from flask import Flask
+import sqlite3
+
+from flask import Flask, g
 
 
-# create a new Flask App
+# Config
+DATABASE = "flaskr.db"
+
+# Create a new Flask App
 app = Flask(__name__)
+
+# Load config
+app.config.from_object(__name__)
+
+
+def connect_db():
+    """Connect to the database"""
+    rv = sqlite3.connect(app.config["DATABASE"])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
+def init_db():
+    """Create the database"""
+    with app.app_context():
+        db = get_db()
+        with app.open_resource("schema.sql", mode="r") as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+def get_db():
+    """Open database connection"""
+    if not hasattr(g, "sqlite_db"):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, "sqlite_db"):
+        g.sqlite_db.close()
 
 
 @app.route('/')
